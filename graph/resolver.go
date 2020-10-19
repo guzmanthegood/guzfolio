f9b40c2491b67fa745ad5632fb10313933da9fd3
Carlos Guzmán <cguzman@xmltravelgate.com>
2020-10-19 12:48:59 +0200

    chi router setup

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,11 @@
 package graph
 
+import "guzfolio/datastore"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct{
+	DS datastore.DataStore
+}
