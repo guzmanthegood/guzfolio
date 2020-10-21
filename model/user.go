b34dc1f05b0b81613c486811d095af7718aabcab
Carlos Guzmán <cguzman@xmltravelgate.com>
2020-10-21 12:13:39 +0200

    isAdmin attribute has been added

--- model/user.go
+++ model/user.go
@@ -1,9 +1,10 @@
 package model
 
 type User struct {
-	ID         uint      	`json:"id"`
-	Email      string       `json:"email" gorm:"unique"`
-	Password   string       `json:"password"`
-	Name       string       `json:"name"`
-	Portfolios []*Portfolio `json:"portfolios"`
+	ID         uint      		`json:"id"`
+	Email      string       	`json:"email" gorm:"unique"`
+	Password   string       	`json:"password"`
+	Name       string       	`json:"name"`
+	IsAdmin	   bool				`json:"isAdmin"`
+	Portfolios []*Portfolio 	`json:"portfolios"`
 }
